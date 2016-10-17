72ba6e8da9f03cbe2b8e1f2d7d9d2d1dab67a8d4
Jishnu Mohan <jishnu7@gmail.com>
2016-10-17 08:03:47 +0530

    2 space intent

--- ios/widget/TodayViewController.m
+++ ios/widget/TodayViewController.m
@@ -17,63 +17,62 @@
 @implementation TodayViewController
 
 - (id)initWithCoder:(NSCoder *)aDecoder {
-    if (self = [super initWithCoder:aDecoder]) {
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(userDefaultsDidChange:)
-                                                     name:NSUserDefaultsDidChangeNotification
-                                                   object:nil];
-    }
-    return self;
+  if (self = [super initWithCoder:aDecoder]) {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                         selector:@selector(userDefaultsDidChange:)
+                           name:NSUserDefaultsDidChangeNotification
+                           object:nil];
+  }
+  return self;
 }
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    [self updateActionLabel];
+  [super viewDidLoad];
+  [self updateActionLabel];
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
-    // Perform any setup necessary in order to update the view.
+  // Perform any setup necessary in order to update the view.
 
-    // If an error is encountered, use NCUpdateResultFailed
-    // If there's no update required, use NCUpdateResultNoData
-    // If there's an update, use NCUpdateResultNewData
+  // If an error is encountered, use NCUpdateResultFailed
+  // If there's no update required, use NCUpdateResultNoData
+  // If there's an update, use NCUpdateResultNewData
 
-    completionHandler(NCUpdateResultNewData);
+  completionHandler(NCUpdateResultNewData);
 }
 
-- (IBAction)launchHostingApp:(id)sender
-{
-    // TODO: use sender.tag to identify the button and send widgetAction based on that
-    NSString *bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
-    bundleIdentifier = [bundleIdentifier stringByReplacingOccurrencesOfString: @".twidget" withString:@""];
+- (IBAction)launchHostingApp:(id)sender {
+  // TODO: use sender.tag to identify the button and send widgetAction based on that
+  NSString *bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
+  bundleIdentifier = [bundleIdentifier stringByReplacingOccurrencesOfString: @".twidget" withString:@""];
 
-    NSString *urlString = [NSString stringWithFormat:@"%@://widget?widgetAction=play", bundleIdentifier];
+  NSString *urlString = [NSString stringWithFormat:@"%@://widget?widgetAction=play", bundleIdentifier];
 
-    NSURL *URL = [NSURL URLWithString:urlString];
-    [self.extensionContext openURL:URL completionHandler:nil];
+  NSURL *URL = [NSURL URLWithString:urlString];
+  [self.extensionContext openURL:URL completionHandler:nil];
 }
 
 - (void)userDefaultsDidChange:(NSNotification *)notification {
-    [self updateActionLabel];
+  [self updateActionLabel];
 }
 
 - (void)updateActionLabel {
-    NSString *appGroup = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"AppGroup"];
+  NSString *appGroup = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"AppGroup"];
 
-    NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:appGroup];
-    NSString *number = [defaults stringForKey:@"play"];
+  NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:appGroup];
+  NSString *number = [defaults stringForKey:@"play"];
 
-    if([number length] == 0) {
-        // TODO: localize this
-        number = @"Play";
-    }
+  if([number length] == 0) {
+    // TODO: localize this
+    number = @"Play";
+  }
 
-    [self.actionButton setTitle:number forState:UIControlStateNormal];
+  [self.actionButton setTitle:number forState:UIControlStateNormal];
 }
 
 @end
